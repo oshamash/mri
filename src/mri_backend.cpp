f5f76a64995dc820eb0ee0cc442f380a0b0ba694
Omer Shamash <oshamash@drivenets.com>
2020-03-16 17:39:08 +0000

    MRI: v1: UMake IMake WeMake it work

--- src/mri_backend.cpp
+++ src/mri_backend.cpp
@@ -1,166 +1,19 @@
 #include <map>
 #include <string>
+#include <vector>
 #include <functional>
 #include <unordered_map>
 
 /* TODO(omer): Fix include paths */
 extern "C" {
-	#include "../include/mri.h"
+	#include "mri.h"
 //	#include "dn_common/general.h"
 }
 
-#include "general.hpp"
+#include "mri_general.hpp"
 //#include "../deps/ordered_map.h"
 
-/* TODO(omer): Change to something real */
-#define mri_assert(cond) (void) 0
-
-/* gettid() is undefined in glibc, use this instead */
-#include <unistd.h>
-#include <sys/syscall.h>
-typedef pid_t tid_t;
-static tid_t _gettid() { return syscall(SYS_gettid); }
-
-/* Declarations (should move soon) */
-struct xtype;
-struct xformat;
-struct xshaper;
-struct mri_slot;
-struct xpath_node;
-
-using mri_slot_map_t = std::map<cstring_t, mri_slot>;
-using mri_subpath_map_t = std::map<cstring_t, xpath_node>;
-using mri_capture_map_t = std::unordered_map<cstring_t, std::vector<mri_capture_sample_t>>;
-using mri_formatter_map_t = std::unordered_map<cstring_t, xformat>;
-using mri_shaper_map_t = std::unordered_map<cstring_t, xshaper>;
-using mri_xtype_map_t = std::unordered_map<cstring_t, xtype>;
-
-struct xformat {
-	std::string			m_name;
-	mri_formatter_cb	m_formatter;
-
-	template <typename string_t>
-	xformat(string_t &&name, mri_formatter_cb formatter)
-	: m_name(std::forward<string_t>(name))
-	, m_formatter(formatter)
-	{}
-};
-
-struct xshaper {
-	std::string		m_name;
-	mri_shaper_cb	m_shaper;
-
-	template <typename string_t>
-	xshaper(string_t &&name, mri_shaper_cb shaper)
-	: m_name(std::forward<string_t>(name))
-	, m_shaper(shaper)
-	{}
-};
-
-/**
- * MRI_SLOT
- * Member of a registered user-defined data structure
- */
-struct mri_slot {
-	std::string			m_name;			/*!< Name of the slot (unique per-xtype)	*/
-	size_t				m_offset;		/*!< Offset of the slot's data in xtype		*/
-	size_t				m_size;			/*!< Size of the slot (used by formatter)	*/
-	mri_formatter_cb	m_formatter;	/*!< Reference to formatter (int, str, ...)	*/
-	mri_shaper_cb		m_shaper;		/*!< Pointer to slot data-samples shaper_cb	*/
-	bool				m_is_hidden;	/*!< Indicator for external usage (to-show)	*/
-
-	/* TODO(omer): Find a better way (m_shaper doubles as is-slot-shaper indicator) */
-	/* Note: m_shaper will be filled externally for xpath_node::xtype::dump to use	*/
-	template <typename string_t>
-	mri_slot(	string_t			&&name,
-				size_t				offset,
-				size_t				size,
-				mri_formatter_cb	formatter,
-				bool				hidden)
-	: m_name(std::forward<string_t>(name))
-	, m_offset(offset)
-	, m_size(size)
-	, m_formatter(formatter)
-	, m_shaper(nullptr)
-	, m_is_hidden(hidden)
-	{}
-};
-
-/* TODO(omer): Consider enabling a flat-parse for an xtype (slot, slot, slot, ...) */
-
-/**
- * XTYPE
- * Type which is composed from multiple slots (complex type)
- * Those types are usually being registered to one or more xpath_node
- */
-struct xtype {
-	std::string			m_name;				/*!< Name of this X-Type (unique)			*/
-	size_t				m_size;				/*!< Size needed to allocate the type		*/
-	mri_slot_map_t		m_slots;			/*!< Ordered mapping of all type's slots	*/
-	struct mri_slot		*m_private_key;		/*!< Explicit pointer to the type's PK slot	*/
-	mri_capture_map_t	m_slot_capture_map;	/*!< Externalize the per-slot data captures	*/
-
-	template <typename string_t>
-	xtype(string_t &&name, size_t size)
-	: m_name(std::forward<string_t>(name))
-	, m_size(size)
-	, m_slots()
-	, m_private_key(nullptr)
-	, m_slot_capture_map()
-	{}
-
-	/**
-	 * Create a slot (data-member)
-	 *	@param name - name of the new slot
-	 *	@param offset - byte offset in data struct (offsetof)
-	 *	@param size - size in bytes of contained data (sizeof)
-	 *	@param formatter - format callback for the new slot (to_string)
-	 *	@param flags - bitwise flags of slot options (pk, rate, hidden, etc...)
-	 *	@return C-Style boolean (0 = OK, <0 = ERROR) (-ERRNO)
-	 */
-	int add_slot(cstring_t name, size_t offset, size_t size, mri_formatter_cb formatter, int flags);
-};
-
-/**
- * xpath_node
- *	Chains of nodes defining path (/ is root)
- */
-struct xpath_node {
-	/* General definition of a node */
-	std::string			m_name;				/*>! Name of node (name unique per parent)	*/
-	mri_subpath_map_t	m_sub_nodes;		/*>! Mapping of sub-nodes (each is unique)	*/
-	xtype				*m_type;			/*>! Type coresponding with the node's data	*/
-	xpath_node			*m_parent;			/*>! Reverse reference to the parent's node	*/
-
-	/* User-data (specified on registration) */
-	tid_t				m_tid;				/*>! Thread whom registered node (origin)	*/
-	void				*m_xdata;			/*>! User-defined data (node data internal)	*/
-	mri_iterator_cb		m_xdata_iterator;	/*>! User-defined data iterator (tableview)	*/
-
-	/**
-	 * Notes:
-	 *	1. No type = empty node, path-building
-	 *	2. No parent = root node, others should not do this
-	 *	3. No data = only allowed for empty node
-	 *	4. Iterator = optional, data can be flat
-	 *	5. Name cannot contain / (except root) to allow path traversal
-	 *	6. Path is defined as <ROOT>/<NODE-1>/<NODE-2>/.../<NODE-X>
-	 */
-	template <typename string_t>
-	xpath_node(	string_t		&&name,
-				xtype			*type,
-				xpath_node		*parent,
-				void			*xdata,
-				mri_iterator_cb	data_iter)
-	: m_name(std::forward<string_t>(name))
-	, m_sub_nodes()
-	, m_type(type)
-	, m_parent(parent)
-	, m_tid(_gettid())
-	, m_xdata(xdata)
-	, m_xdata_iterator(data_iter)
-	{ mri_assert(parent || ! data);  }
-};
+#include "mri_data.hpp"
 
 /* Global variables */
 static mri_xtype_map_t		mri_xtypes;
@@ -172,11 +25,9 @@ static xpath_node			mri_root ("/", nullptr, nullptr, nullptr, nullptr);
 	/* Note: ignore potential copy, even by using std::move, compiler optimization will fix this */	\
 	/* Note: usage of m_name.c_str() as the map-key allows us to avoid another string allocation */	\
 	decltype(map)::mapped_type __temp_cls (key, ##__VA_ARGS__);										\
-	auto&& [iter, did_insert] = map.try_emplace(__temp_cls.m_name.c_str(), std::move(__temp_cls))	\
+	auto&& [iter, did_insert] = map.try_emplace(__temp_cls.m_name.c_str(), std::move(__temp_cls))
 
-#define MRI_SAFE_MAP_INSERTION(exists_err, map_data_t, map, key, ...)								\
-	MRI_MAP_INSERT(map, key, ##__VA_ARGS__);														\
-																									\
+#define MRI_MAP_INSERT_CHECK_RES(exists_err, map_data_t, map, key)									\
 	if (likely(did_insert)) return 0; /* Bailout if all worked */									\
 																									\
 	if (map.end() == iter) {																		\
@@ -187,14 +38,21 @@ static xpath_node			mri_root ("/", nullptr, nullptr, nullptr, nullptr);
 	MRI_LOG(MRI_WARNING, #map_data_t " %s already exists", key);									\
 	return exists_err
 
-int xtype::add_slot(cstring_t name, size_t offset, size_t size, mri_formatter_cb formatter, int flags) {
-	(void) name;
-	(void) offset;
-	(void) size;
-	(void) formatter;
-	(void) flags;
+#define MRI_SAFE_MAP_INSERTION(exists_err, map_data_t, map, key, ...)								\
+	MRI_MAP_INSERT(map, key, ##__VA_ARGS__);														\
+	MRI_MAP_INSERT_CHECK_RES(exists_err, map_data_t, map, key)
 
-	return 0;
+/* Easier to implement here due to macros, consider moving */
+int xtype::add_slot(cstring_t slot, size_t offset, size_t size, mri_formatter_cb formatter, int flags) {
+	MRI_SAFE_MAP_INSERTION(	-EEXIST,
+							mri_slot,
+							m_slots,
+							/* mri_slot construction */
+							slot,
+							offset,
+							size,
+							formatter,
+							(flags & MRI_SLOT_FLAG_HIDDEN));
 }
 
 extern "C" {
@@ -305,7 +163,7 @@ extern "C" {
 	}
 
 	int _mri_type_add_shaper(	cstring_t	type,
-								cstring_t	name,
+								cstring_t	slot,
 								cstring_t	shaper,
 								size_t		offset,
 								size_t		size) {
@@ -324,15 +182,15 @@ extern "C" {
 		auto &mri_type = xtype_iter->second;
 
 		/* Add hidden slot for shaping (allow for user to add it as not-hidden before) */
-		int retval = mri_type.add_slot(name, offset, size, nullptr, MRI_SLOT_FLAG_HIDDEN);
+		int retval = mri_type.add_slot(slot, offset, size, nullptr, MRI_SLOT_FLAG_HIDDEN);
 		if (unlikely(0 != retval && -EEXIST != retval)) return retval;
 
 		/* Save shaper to use later in xdump */
 		auto const &xshaper = shaper_iter->second;
-		auto xslot_iter = mri_type.m_slots.find(name);
-		xslot_iter->second.m_shaper = xshaper.m_shaper;
-
-		return 0;
+		auto &xslot = mri_type.m_slots.find(slot)->second;
+		xslot.m_shaper = xshaper.m_shaper;
+		auto&& [iter, did_insert] = mri_type.m_slot_capture_map.try_emplace(slot, slot_capture_data {});
+		MRI_MAP_INSERT_CHECK_RES(-EEXIST, slot_capture_data, mri_type.m_slot_capture_map, slot);
 	}
 
 	/* ---------------------------------------------------------------------------------------------
